a418a56170f4a5ce2cd9c190d1a8ce08f9ee4e4d
Arty Gus <hero@engineeriam.net>
2025-11-23 11:44:29 +0000

    add homepage

--- Formula/mdkir.rb
+++ Formula/mdkir.rb
@@ -1,5 +1,6 @@
 class Mdkir < Formula
   desc "Prints Kir if you type mdkir instead of mkdir"
+  homepage "https://github.com/artygus/mdkir"
   license "MIT"
   head "https://github.com/artygus/mdkir.git", branch: "main"
 
